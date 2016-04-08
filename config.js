bd4c3032e80ff70cc1014f9f4eb86c54361b3a9b
Steven Harradine <stevenharradine@gmail.com>
2016-04-07 23:08:24 -0400

    write issues to database

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports.DATABASE_HOST          = "localhost"
+module.exports.DATABASE_USER          = "csp"
+module.exports.DATABASE_PASSWORD      = "super-secure-password"
+module.exports.DATABASE_DATABASE_NAME = "csp"
--- server.js
+++ server.js
@@ -1,30 +1,53 @@
-var http = require('http');
-var PORT = 8080;
+var CONFIG = require("./config"),
+    http   = require('http'),
+    mysql  = require('mysql'),
+    PORT   = 8080
 
-var server = http.createServer(function (request, response){
-    var body = ""
-
+var server = http.createServer(function (request, response) {
     if (request.method == 'POST') {
+        var body = ""
+
         request.addListener('data', function(chunk){
-          body += chunk;
+          body += chunk
         });
 
         request.addListener('end', function(chunk){
           if (chunk) {
-            body += chunk;
+            body += chunk
           }
 
-          console.log (body);
+          csp_report = JSON.parse (body)['csp-report'];
+          
+          var connection = mysql.createConnection ({
+            host     : CONFIG.DATABASE_HOST,
+            user     : CONFIG.DATABASE_USER,
+            password : CONFIG.DATABASE_PASSWORD,
+            database : CONFIG.DATABASE_DATABASE_NAME
+          });
 
-          response.writeHead(200, {'Content-Type': 'text/html'});
-          response.end('');
-        });
+          var document_uri       = connection.escape (csp_report['document-uri']),
+              referrer           = connection.escape (csp_report['referrer']),
+              voilated_directive = connection.escape (csp_report['voilated-directive']) === "NULL" ? "''" : connection.escape (csp_report['voilated-directive']),
+              original_policy    = connection.escape (csp_report['original-policy']),
+              blocked_uri        = connection.escape (csp_report['blocked-uri']),
+              date               = "CURRENT_TIMESTAMP",
+              sql                = "INSERT INTO `csp` (`document_uri`, `referrer`, `violated-directive`, `original-policy`, `blocked-uri`, `date`) VALUES (" + document_uri + ", " + referrer + ", " + voilated_directive + ", " + original_policy + ", " + blocked_uri + ", " + date + ");"
+
+          connection.query (sql, function (error, rows, fields) {
+            if (error)
+              console.log ('Error:' + error)
+          })
+          connection.end ()
+
+          response.writeHead (200, {'Content-Type': 'text/html'})
+          response.end ('')
+        })
     } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.end("noop ... try POST");
+        response.writeHead (404)
+        response.end ('')
     }
 });
 
-server.listen(PORT, '0.0.0.0', function(){
-    console.log("Server listening on: http://localhost:%s", PORT);
+server.listen (PORT, '0.0.0.0', function () {
+    console.log("Server listening on: http://localhost:%s", PORT)
 });
