b2ccf85cf42bd3024edf761e7a21a351e8d49978
Steven Harradine <stevenharradine@gmail.com>
2016-04-12 11:24:59 -0400

    error check json blob that should just be the csp report but apparently might not

--- server.js
+++ server.js
@@ -16,28 +16,32 @@ var server = http.createServer(function (request, response) {
             body += chunk
           }
 
-          csp_report = JSON.parse (body)['csp-report'];
-          
-          var connection = mysql.createConnection ({
-            host     : CONFIG.DATABASE_HOST,
-            user     : CONFIG.DATABASE_USER,
-            password : CONFIG.DATABASE_PASSWORD,
-            database : CONFIG.DATABASE_DATABASE_NAME
-          });
+          try {
+            csp_report = JSON.parse (body)['csp-report'];
+            
+            var connection = mysql.createConnection ({
+              host     : CONFIG.DATABASE_HOST,
+              user     : CONFIG.DATABASE_USER,
+              password : CONFIG.DATABASE_PASSWORD,
+              database : CONFIG.DATABASE_DATABASE_NAME
+            });
 
-          var document_uri       = escape_not_null (csp_report['document-uri'], connection),
-              referrer           = escape_not_null (csp_report['referrer'], connection),
-              violated_directive = escape_not_null (csp_report['violated-directive'], connection),
-              original_policy    = escape_not_null (csp_report['original-policy'], connection),
-              blocked_uri        = escape_not_null (csp_report['blocked-uri'], connection),
-              date               = "CURRENT_TIMESTAMP",
-              sql                = "INSERT INTO `csp` (`document_uri`, `referrer`, `violated-directive`, `original-policy`, `blocked-uri`, `date`) VALUES (" + document_uri + ", " + referrer + ", " + violated_directive + ", " + original_policy + ", " + blocked_uri + ", " + date + ");"
+            var document_uri       = escape_not_null (csp_report['document-uri'], connection),
+                referrer           = escape_not_null (csp_report['referrer'], connection),
+                violated_directive = escape_not_null (csp_report['violated-directive'], connection),
+                original_policy    = escape_not_null (csp_report['original-policy'], connection),
+                blocked_uri        = escape_not_null (csp_report['blocked-uri'], connection),
+                date               = "CURRENT_TIMESTAMP",
+                sql                = "INSERT INTO `csp` (`document_uri`, `referrer`, `violated-directive`, `original-policy`, `blocked-uri`, `date`) VALUES (" + document_uri + ", " + referrer + ", " + violated_directive + ", " + original_policy + ", " + blocked_uri + ", " + date + ");"
 
-          connection.query (sql, function (error, rows, fields) {
-            if (error)
-              console.log ('Error:' + error)
-          })
-          connection.end ()
+            connection.query (sql, function (error, rows, fields) {
+              if (error)
+                console.log ('Error:' + error)
+            })
+            connection.end ()
+          } catch (error) {
+            console.log ("Error: " + error)
+          }
 
           response.writeHead (200, {'Content-Type': 'text/html'})
           response.end ('')
